34d9e78e83a90db0b185ba6af67f88220f3fc89a
Hounnankan7 <95772317+Hounnankan7@users.noreply.github.com>
2022-09-01 14:49:14 +0200

    update des zones

--- scripts/interfaces/interface_inGame.lua
+++ scripts/interfaces/interface_inGame.lua
@@ -14,19 +14,18 @@
     function inGame_Init()
 
         ui_inGame = love.graphics.newImage("assets/interfaces/05_inGame.png")
-        zone_1 = {x = 7.3, y = 12.3}
-        zone_2 = {x = 14, y = 14.3}
-        zone_3 = {x = 15, y = 22.6}
-        zone_4 = {x = 6.3, y = 34.7}
-        zone_5 = {x = 25, y = 24}
-        
-        zone_6 = {x = 1, y = 1}
-        zone_7 = {x = 1, y = 1}
-        zone_8 = {x = 1, y = 1}
-        zone_9 = {x = 1, y = 1}
-        zone_10 = {x = 1, y = 1}
-        zone_11 = {x = 1, y = 1}
-        zone_12 = {x = 1, y = 1}
+        zone_1 = {x = 7.3, y = 12.3, state = 0} -- Ship Debris
+        zone_2 = {x = 14, y = 14.3, state = 0} -- Rice Village
+        zone_3 = {x = 15, y = 22.6, state = 0} -- Ancient Tent
+        zone_4 = {x = 6.3, y = 34.7, state = 0} -- Stone Mine
+        zone_5 = {x = 25, y = 24, state = 0} -- Ice Mine
+        zone_6 = {x = 33.2, y = 16, state = 0} -- Sky Forest
+        zone_7 = {x = 39, y = 25, state = 0} -- Babylon Garden
+        zone_8 = {x = 33, y = 36.6, state = 0} -- Big Crate
+        zone_9 = {x = 52.3, y = 12.3, state = 0} -- Canyon
+        zone_10 = {x = 53, y = 17, state = 0} -- Heavy Dune
+        zone_11 = {x = 54, y = 22, state = 0} -- Golden Pyramide
+        zone_12 = {x = 54, y = 34, state = 0} -- Vortex
 
     end
 
@@ -49,6 +48,26 @@
         love.graphics.rectangle("fill", zone_5.x*tileSize, zone_5.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- ice mine
         love.graphics.rectangle("line", (zone_5.x-0.2)*tileSize, (zone_5.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
 
+        love.graphics.rectangle("fill", zone_6.x*tileSize, zone_6.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- sky forest
+        love.graphics.rectangle("line", (zone_6.x-0.2)*tileSize, (zone_6.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_7.x*tileSize, zone_7.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_7.x-0.2)*tileSize, (zone_7.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_8.x*tileSize, zone_8.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_8.x-0.2)*tileSize, (zone_8.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_9.x*tileSize, zone_9.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_9.x-0.2)*tileSize, (zone_9.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_10.x*tileSize, zone_10.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_10.x-0.2)*tileSize, (zone_10.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_11.x*tileSize, zone_11.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_11.x-0.2)*tileSize, (zone_11.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
+
+        love.graphics.rectangle("fill", zone_12.x*tileSize, zone_12.y*tileSize, 1.5*tileSize, 1.5*tileSize) -- babylon garden
+        love.graphics.rectangle("line", (zone_12.x-0.2)*tileSize, (zone_12.y-0.2)*tileSize, 2*tileSize, 2*tileSize)
 
     end
 
